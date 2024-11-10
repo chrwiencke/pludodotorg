0ecc1bbfb6c24e235a1ff375774c557a3939ec69
chrwiencke <81775007+chrwiencke@users.noreply.github.com>
2024-11-10 21:59:20 +0100

    Change output configuration from 'server' to 'hybrid'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
